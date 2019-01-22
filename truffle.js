a4ecee7f06929b78c0b1eb60a12e597a978c27eb
Vladimir Komendantskiy <komendantsky@gmail.com>
2019-01-22 17:40:32 +0000

    A template for integration tests using Truffle

---
+++ truffle.js
@@ -0,0 +1,19 @@
+module.exports = {
+  networks: {
+    node1: {
+      host: "localhost",
+      port: 8541,
+      network_id: 101,
+    },
+    node2: {
+      host: "localhost",
+      port: 8542,
+      network_id: 101
+    },
+    node3: {
+      host: "localhost",
+      port: 8543,
+      network_id: 101
+    }
+  }
+};
