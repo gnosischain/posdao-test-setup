9a9461bb38ab094658ff97bb5838e258367b4363
Demi M. Obenour <demiobenour@gmail.com>
2019-01-23 20:16:22 -0500

    Add validator balances to spec.json. Update submodules.
    Adding an option to `make_spec.js` in the `pos-contracts` repo to give
    initial balances to validators would be a mistake, since a mistake could
    allow a network to go into production with the validators having
    balances that they should not have.  However, tests require the
    validators to have a balance, to pay for gas.  The solution is to
    generate a private, ephemeral copy of `spec.json` from the one generated
    by the upstream `make_spec.js`.  This is done by
    `scripts/add-validator-balances.js`.
    
    Additionally, several cleanups are made to `scripts/getReservedPeer.js`,
    the `pos-contracts` submodule is updated, and `scripts/start-test-setup`
    and `package.json` are changed so that
    `scripts/add-validator-balances.js` is run with the correct environment
    at the correct time.

--- scripts/getReservedPeer.js
+++ scripts/getReservedPeer.js
@@ -1,13 +1,31 @@
 const fs = require('fs');
+const { promisify } = require('util');
+const appendFile = promisify(fs.appendFile);
+const { exec } = require('child_process');
 const { URL } = require('url');
 const process = require('process');
 var os = require("os");
 
 main();
 
+const isObject = x => x !== null && typeof x === 'object' && !Array.isArray(x);
+const process = require('process');
+const os = require("os");
 async function main() {
+  inner_main().catch(e => {
+    console.error(e);
+    process.exitCode = 1;
+  });
+}
+
+async function inner_main() {
   const maxAttempts = 5;
-  var node_index = process.argv[2].toString();
+  const node_index = process.argv[2].toString();
+
+  // Stop any injection attacks
+  const whitelist = /^[0-9]+$/s;
+  if (!whitelist.test(node_index) || typeof node_index !== 'string')
+    throw new Error('Invalid node index');
   console.log("Registering node " + node_index + " as reserved peer");
   const cmd = `curl --data '{"method":"parity_enode","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:854`
         + node_index
@@ -17,13 +35,15 @@ async function main() {
     try {
       const enodeURL = await getEnodeURL(cmd);
       console.log("enode URL: " + enodeURL);
-      fs.appendFileSync("parity-data/reserved-peers", enodeURL + os.EOL);
+      await appendFile(__dirname + "/../parity-data/reserved-peers", enodeURL + os.EOL);
       break;
     } catch(e) {
       if (i <= maxAttempts) {
         await sleep(5000);
       } else {
-        console.log(e.message);
+        console.log(e);
+        console.error(e);
+        process.exitCode = 1;
       }
     }
   }
@@ -31,30 +51,36 @@ async function main() {
 
 function getEnodeURL(cmd) {
   return new Promise((resolve, reject) => {
-    var exec = require('child_process').exec;
     exec(cmd, function (error, stdout, stderr) {
       if (error !== null) {
         reject(error);
       }
-      let resp;
       try {
-        resp = JSON.parse(stdout);
-      } catch(e) {
-        reject(e);
-      }
-      let result;
-      try {
-        if (resp.result) {
-          result = new URL(resp.result);
-          result.host = '127.0.0.1';
-          result = result.href;
-        } else {
-          throw new Error('result is undefined');
+        const resp = JSON.parse(stdout);
+        for (let i in resp) {
+          switch (i) {
+          case 'error':
+            throw new Error(typeof resp.error === 'string' ? resp.error : JSON.stringify(resp.error));
+          case 'result':
+          case 'jsonrpc':
+          case 'id':
+            break;
+          default:
+            throw new SyntaxError('unknown field in JSON-RPC response');
+          }
         }
+        if (1 !== resp.id)
+          throw new SyntaxError('invalid JSON-RPC response ID');
+        if ('2.0' !== resp.jsonrpc)
+          throw new SyntaxError('invalid JSON-RPC response version');
+        if (typeof resp.result !== 'string')
+          throw new TypeError('wrong type for resp.result');
+        const result = new URL(resp.result);
+        result.host = '127.0.0.1';
+        resolve(result.href);
       } catch (e) {
         reject(e);
       }
-      resolve(result);
     });
   })
 }
@@ -62,3 +88,5 @@ function getEnodeURL(cmd) {
 function sleep(millis) {
   return new Promise(resolve => setTimeout(resolve, millis));
 }
+
+main();
