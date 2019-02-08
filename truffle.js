c4ab45f75b9859a4a0f933cfcd74260bf503d07b
Vadim Arasev <33550681+varasev@users.noreply.github.com>
2019-02-08 10:28:19 +0300

    Reduce gas limit in truffle.js

--- truffle.js
+++ truffle.js
@@ -9,7 +9,7 @@ module.exports = {
       port: 8541,
       network_id: "*",
       from: "0x32e4e4c7c5d1cea5db5f9202a9e4d99e56c91a24",
-      gas: 8000000,
+      gas: 6000000,
       gasPrice: 0,
       before_timeout: 300000,  // test timeout in ms
       test_timeout: 300000     // test timeout in ms
