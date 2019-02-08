116cb67228b149fa031cd39ec573a863173171d9
phahulin <p.hahulin@hintsolutions.ru>
2019-02-09 01:09:53 +0300

    Rename VALIDATOR_SET_BASE_ABI to just abi

--- utils/getContract.js
+++ utils/getContract.js
@@ -3,11 +3,11 @@ const constants = require("./constants");
 
 module.exports = function (contractName, web3) {
   if (contractName === "ValidatorSetAuRa") {
-    const VALIDATOR_SET_BASE_ABI = require("../posdao-contracts/build/contracts/ValidatorSetAuRa").abi;
+    const abi = require("../posdao-contracts/build/contracts/ValidatorSetAuRa").abi;
     return {
       address: constants.VALIDATOR_SET_ADDRESS,
-      abi: VALIDATOR_SET_BASE_ABI,
-      instance: new web3.eth.Contract(VALIDATOR_SET_BASE_ABI, constants.VALIDATOR_SET_ADDRESS),
+      abi: abi,
+      instance: new web3.eth.Contract(abi, constants.VALIDATOR_SET_ADDRESS),
     };
   }
   else {
