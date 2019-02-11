11415b224ca70b56936e25e98c4327ba2d1b97e7
Vladimir Komendantskiy <komendantsky@gmail.com>
2019-02-11 22:20:14 +0000

    unit test for minting tokens

--- test/token.js
+++ test/token.js
@@ -1,8 +1,9 @@
+const BN = require('bn.js');
 const Token = artifacts.require('ERC677BridgeTokenRewardableMock')
 
 const ValidatorSetContract = require("../utils/getContract")("ValidatorSetAuRa", web3);
 
-contract('TestToken', _accounts => {
+contract('TestToken', async accounts => {
   it('should have 0 initial supply', async () => {
     var instance = await Token.deployed()
     var supply = await instance.totalSupply.call()
@@ -19,5 +20,21 @@ contract('TestToken', _accounts => {
     let instance = await Token.deployed();
     let erc20TokenContract = await ValidatorSetContract.instance.methods.erc20TokenContract().call();
     assert.equal(erc20TokenContract.valueOf(), instance.address);
+  });
+
+  it('should mint tokens', async () => {
+    assert(accounts.length > 0, 'no accounts to carry on the test');
+    let instance = await Token.deployed();
+    const increment = 1;
+    const incrementBN = new BN(increment.toString()); // web3.utils.toWei('1');
+    for (var i = 0; i < accounts.length; i++) {
+      const addr = accounts[i];
+      console.log('   *** Minting tokens for account ' + addr.toString());
+      const balanceBefore = await instance.balanceOf.call(addr);
+      await instance.mint.call(addr, incrementBN);
+      const balanceAfter = await instance.balanceOf.call(addr);
+      assert.equal(balanceBefore.valueOf().add(incrementBN), balanceAfter.valueOf(),
+                   'balances before minting and after do not differ by the minted amount.');
+    }
   });
 })
