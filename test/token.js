f1454b77e626072db6ae362bce879e506fd506ba
Vladimir Komendantskiy <komendantsky@gmail.com>
2019-02-12 17:01:23 +0000

    test minting tokens to candidates

--- test/token.js
+++ test/token.js
@@ -1,23 +1,41 @@
-const Token = artifacts.require('ERC677BridgeTokenRewardableMock')
-
+const BN = web3.utils.BN;
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bn')(BN))
+  .should();
+const Token = artifacts.require('ERC677BridgeTokenRewardableMock');
 const ValidatorSetContract = require("../utils/getContract")("ValidatorSetAuRa", web3);
+const constants = require('../utils/constants');
 
-contract('TestToken', _accounts => {
+contract('TestToken', async accounts => {
+  let instance;
+
   it('should have 0 initial supply', async () => {
-    var instance = await Token.deployed()
-    var supply = await instance.totalSupply.call()
-    assert.equal(supply.valueOf(), 0, "the initial supply isn't 0")
-  })
+    instance = await Token.deployed();
+    let supply = await instance.totalSupply.call();
+    assert.equal(supply.valueOf(), 0, "the initial supply isn't 0");
+  });
 
   it('validatorSetContract field value should match ValidatorSet contract address', async () => {
-    let instance = await Token.deployed();
+    instance = await Token.deployed();
     let validatorSetContractAddress = await instance.validatorSetContract.call();
     assert.equal(validatorSetContractAddress.valueOf(), ValidatorSetContract.address);
   });
 
   it('deployed address should match erc20TokenContract in ValidatorSet contract', async () => {
-    let instance = await Token.deployed();
+    instance = await Token.deployed();
     let erc20TokenContract = await ValidatorSetContract.instance.methods.erc20TokenContract().call();
     assert.equal(erc20TokenContract.valueOf(), instance.address);
+  });
+
+  it('should mint staking tokens to candidates', async () => {
+    instance = await Token.deployed();
+    const candidateStake = new BN(web3.utils.toWei('1', 'ether').toString());
+    for (candidate of constants.CANDIDATES) {
+    const balanceBefore = await instance.balanceOf(candidate);
+      await instance.mint(candidate, candidateStake).should.be.fulfilled;
+      const balanceAfter = await instance.balanceOf(candidate);
+      balanceAfter.should.be.bignumber.equal(balanceBefore.add(candidateStake));
+    }
   });
 })
--- utils/constants.js
+++ utils/constants.js
@@ -3,5 +3,6 @@ const VALIDATOR_SET_ADDRESS = require("../posdao-contracts/spec").engine.authori
 
 module.exports = {
   OWNER: "0x32e4e4c7c5d1cea5db5f9202a9e4d99e56c91a24",
+  CANDIDATES: ["0xf67cc5231c5858ad6cc87b105217426e17b824bb", "0xbe69eb0968226a1808975e1a1f2127667f2bffb3", "0x720e118ab1006cc97ed2ef6b4b49ac04bb3aa6d9"],
   VALIDATOR_SET_ADDRESS,
 };
