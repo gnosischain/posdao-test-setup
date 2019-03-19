fa68ea76889409a63d22eb4bcb03f285d922ba1f
Vladimir Komendantskiy <komendantsky@gmail.com>
2019-03-19 12:41:27 +0000

    fixed errors in the watcher script

--- scripts/watchBackedUpNode.js
+++ scripts/watchBackedUpNode.js
@@ -6,30 +6,32 @@ var ethers = require("ethers");
 
 const SIGNER_ADDRESS = "0x522df396ae70a058bd69778408630fdb023389b2";
 const PORT = "8546";
-
-var Web3 = require("web3");
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:" + PORT));
-var provider = new ethers.providers.JsonRpcProvider("http://localhost:" + PORT);
-
+const rpcUrl = "http://localhost:" + PORT;
 const RETRY_TIMEOUT_SECONDS = 2;
 const SCAN_INTERVAL_SECONDS = 5;
 const MAX_VALIDATOR_SET_SIZE = 21;
 
+var Web3 = require("web3");
+var web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl));
+var provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 // `true` if the primary is required to sign and `false` if the secondary does.
 var primaryHasToSign = true;
 
 async function scanBlocks() {
-    var lastBlockNum = web3.eth.getBlock("latest").number;
-    if (startBlockNum < MAX_VALIDATOR_SET_SIZE) {
+    var lastBlock = await web3.eth.getBlock("latest");
+    var lastBlockNum = lastBlock.number;
+    assert(typeof lastBlockNum === "number");
+    if (lastBlockNum < MAX_VALIDATOR_SET_SIZE) {
         return true;
     }
     var startBlockNum = lastBlockNum - MAX_VALIDATOR_SET_SIZE;
 
-    console.log(`Scanning blocks from ${startBlockNum} to ${lastBlockNum}.`);
+    console.log(`Scanning blocks from ${startBlockNum} to ${lastBlockNum}`);
 
     for (var i = startBlockNum;  i <= lastBlockNum; i++) {
         let block = await web3.eth.getBlock(i);
         if (block.author === SIGNER_ADDRESS) {
+            console.log("Found a signed block");
             return true;
         }
     }
@@ -63,11 +65,14 @@ function startScan() {
             if (!signed) {
                 console.log(`Failed to find a block authored by ${SIGNER_ADDRESS}`);
                 if (primaryHasToSign) {
-                    await startSecondarySigning();
                     primaryHasToSign = false;
-                } else {
-                    await stopSecondarySigning();
+                    await startSecondarySigning();
+                }
+            } else {
+                console.log(`Found a block authored by ${SIGNER_ADDRESS}`);
+                if (!primaryHasToSign) {
                     primaryHasToSign = true;
+                    await stopSecondarySigning();
                 }
             }
             setTimeout(startScan, SCAN_INTERVAL_SECONDS * 1000);
