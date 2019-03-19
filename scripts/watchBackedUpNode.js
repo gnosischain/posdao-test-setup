449aacd437f2f7c10d5bf37d53ffd2a4eceb7e88
Vladimir Komendantskiy <komendantsky@gmail.com>
2019-03-19 14:01:12 +0000

    monitoring connections to both the primary and the secondary

--- scripts/watchBackedUpNode.js
+++ scripts/watchBackedUpNode.js
@@ -5,20 +5,21 @@ var readFile = promisify(fs.readFile);
 var ethers = require("ethers");
 
 const SIGNER_ADDRESS = "0x522df396ae70a058bd69778408630fdb023389b2";
-const PORT = "8546";
-const rpcUrl = "http://localhost:" + PORT;
+const PORT1 = "8543";
+const PORT2 = "8546";
 const RETRY_TIMEOUT_SECONDS = 2;
 const SCAN_INTERVAL_SECONDS = 5;
 const MAX_VALIDATOR_SET_SIZE = 21;
 
 var Web3 = require("web3");
-var web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl));
-var provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+var web3_1 = new Web3(new Web3.providers.HttpProvider("http://localhost:" + PORT1));
+var web3_2 = new Web3(new Web3.providers.HttpProvider("http://localhost:" + PORT2));
+var provider = new ethers.providers.JsonRpcProvider("http://localhost:" + PORT2);
 // `true` if the primary is required to sign and `false` if the secondary does.
 var primaryHasToSign = true;
 
 async function scanBlocks() {
-    var lastBlock = await web3.eth.getBlock("latest");
+    var lastBlock = await web3_2.eth.getBlock("latest");
     var lastBlockNum = lastBlock.number;
     assert(typeof lastBlockNum === "number");
     if (lastBlockNum < MAX_VALIDATOR_SET_SIZE) {
@@ -29,9 +30,8 @@ async function scanBlocks() {
     console.log(`Scanning blocks from ${startBlockNum} to ${lastBlockNum}`);
 
     for (var i = startBlockNum;  i <= lastBlockNum; i++) {
-        let block = await web3.eth.getBlock(i);
+        let block = await web3_2.eth.getBlock(i);
         if (block.author === SIGNER_ADDRESS) {
-            console.log("Found a signed block");
             return true;
         }
     }
@@ -58,9 +58,13 @@ async function stopSecondarySigning() {
     );
 }
 
-function startScan() {
-    web3.eth.net.isListening()
-        .then(async () => {
+async function startScan() {
+    let secondaryListening = await web3_2.eth.net.isListening();
+    assert(typeof secondaryListening === "boolean");
+    if (secondaryListening) {
+        let primaryListening = await web3_1.eth.net.isListening();
+        assert(typeof primaryListening === "boolean");
+        if (!primaryListening) {
             let signed = await scanBlocks();
             if (!signed) {
                 console.log(`Failed to find a block authored by ${SIGNER_ADDRESS}`);
@@ -68,19 +72,23 @@ function startScan() {
                     primaryHasToSign = false;
                     await startSecondarySigning();
                 }
-            } else {
-                console.log(`Found a block authored by ${SIGNER_ADDRESS}`);
-                if (!primaryHasToSign) {
-                    primaryHasToSign = true;
-                    await stopSecondarySigning();
-                }
             }
-            setTimeout(startScan, SCAN_INTERVAL_SECONDS * 1000);
-        })
-        .catch(e => {
-            console.log(`Not connected to the node; retry in ${RETRY_TIMEOUT_SECONDS} seconds`);
-            setTimeout(startScan, RETRY_TIMEOUT_SECONDS * 1000);
-        });
+        } else {
+            if (!primaryHasToSign) {
+                console.log("Primary has come back up; moving the secondary to reserve");
+                primaryHasToSign = true;
+                await stopSecondarySigning();
+            }
+        }
+    } else {
+        console.log(`Not connected to the secondary; retry in ${RETRY_TIMEOUT_SECONDS}s`);
+        return setTimeout(startScan, RETRY_TIMEOUT_SECONDS * 1000);
+    }
+    setTimeout(startScan, SCAN_INTERVAL_SECONDS * 1000);
 }
 
-startScan();
+try {
+    startScan();
+} catch(e) {
+    console.log("startScan: " + e);
+}
