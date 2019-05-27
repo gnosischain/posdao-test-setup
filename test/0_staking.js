1809dc5d86c39125baa1e8135d7ca169d0ffff09
Vadim <varasev@gmail.com>
2019-05-27 08:52:43 +0300

    Add delegators staking test

--- test/0_staking.js
+++ test/0_staking.js
@@ -11,12 +11,14 @@ const expect = require('chai')
     .use(require('chai-bn')(BN))
     .use(require('chai-as-promised'))
     .expect;
+const BlockRewardAuRa = require('../utils/getContract')('BlockRewardAuRa', web3);
 const ValidatorSetAuRa = require('../utils/getContract')('ValidatorSetAuRa', web3);
 const StakingAuRa = require('../utils/getContract')('StakingAuRa', web3);
 const StakingTokenContract = require('../utils/getContract')('StakingToken', web3);
 const sendInStakingWindow = require('../utils/sendInStakingWindow');
 const waitForValidatorSetChange = require('../utils/waitForValidatorSetChange');
 const pp = require('../utils/prettyPrint');
+const keythereum = require('keythereum');
 
 describe('Candidates make stakes on themselves', () => {
     var minStake;
@@ -89,6 +91,118 @@ describe('Candidates make stakes on themselves', () => {
             let fStake = await StakingAuRa.instance.methods.stakeAmount(candidate.staking, candidate.staking).call();
             let fStakeBN = new BN(fStake.toString());
             expect(fStakeBN, `Stake on candidate ${candidate.staking} didn't increase`).to.be.bignumber.equal(iStakeBN.add(stakeBN));
+        }
+    });
+
+    it('Delegators place stakes into the second candidate\'s pool', async () => {
+        const candidate = constants.CANDIDATES[1].staking;
+
+        console.log('**** Delegator addresses are generated');
+
+        const delegatorsNumber = 10;
+        let delegators = [];
+
+        for (let i = 0; i < delegatorsNumber; i++) {
+            keythereum.create({}, function (dk) {
+                keythereum.dump("testnetpoa", dk.privateKey, dk.salt, dk.iv, {}, function (keyObject) {
+                    keythereum.exportToFile(keyObject, "./accounts/keystore", function(keyFile) {
+                        delegators.push(keyObject.address);
+                    });
+                });
+            });
+        }
+
+        while (delegators.length < delegatorsNumber) {
+            await new Promise(r => setTimeout(r, 100));
+        }
+
+        const minStake = await StakingAuRa.instance.methods.getDelegatorMinStake().call();
+        const minStakeBN = new BN(minStake.toString());
+
+        let promises;
+        let nonce;
+        let txs;
+
+        console.log('**** Owner mints (3x minStake) tokens to delegators');
+
+        const delegatorTokensBN = minStakeBN.mul(new BN('3'));
+
+        promises = [];
+        nonce = await web3.eth.getTransactionCount(OWNER);
+        for (let i = 0; i < delegatorsNumber; i++) {
+            const delegator = delegators[i];
+            const prm = SnS(web3, {
+                from: OWNER,
+                to: StakingTokenContract.address,
+                method: StakingTokenContract.instance.methods.mint(delegator, delegatorTokensBN.toString()),
+                gasPrice: '0',
+                nonce: nonce++
+            });
+            promises.push(prm);
+        }
+        txs = await Promise.all(promises);
+        for (const tx of txs) {
+            expect(tx.status, `Failed tx: ${tx.transactionHash}`).to.equal(true);
+        }
+
+        console.log('**** BlockReward mints native coins to delegators');
+
+        const newNativeBalance = '1000000000000000000';
+
+        await SnS(web3, {
+            from: OWNER,
+            to: BlockRewardAuRa.address,
+            method: BlockRewardAuRa.instance.methods.setErcToNativeBridgesAllowed([OWNER]),
+            gasPrice: '0'
+        });
+
+        promises = [];
+        nonce = await web3.eth.getTransactionCount(OWNER);
+        for (let i = 0; i < delegatorsNumber; i++) {
+            const delegator = delegators[i];
+            const prm = SnS(web3, {
+                from: OWNER,
+                to: BlockRewardAuRa.address,
+                method: BlockRewardAuRa.instance.methods.addExtraReceiver(newNativeBalance, delegator),
+                gasPrice: '0',
+                nonce: nonce++
+            });
+            promises.push(prm);
+        }
+        txs = await Promise.all(promises);
+        for (const tx of txs) {
+            expect(tx.status, `Failed tx: ${tx.transactionHash}`).to.equal(true);
+        }
+
+        for (let i = 0; i < delegatorsNumber; i++) {
+            const delegator = delegators[i];
+            const delegatorBalance = await web3.eth.getBalance(delegator);
+            expect(delegatorBalance, `Amount of minted coins is incorrect for ${delegator}`).to.be.equal(newNativeBalance);
+        }
+
+        console.log('**** Delegators place stakes on the candidate');
+
+        promises = [];
+        for (let i = 0; i < delegatorsNumber; i++) {
+            const delegator = delegators[i];
+            const prm = SnS(web3, {
+                from: delegator,
+                to: StakingAuRa.address,
+                method: StakingAuRa.instance.methods.stake(candidate, minStakeBN.toString()),
+                gasPrice: '1000000000',
+                gasLimit: '200000'
+            });
+            promises.push(prm);
+        }
+        txs = await Promise.all(promises);
+        for (const tx of txs) {
+            expect(tx.status, `Failed tx: ${tx.transactionHash}`).to.equal(true);
+        }
+
+        for (let i = 0; i < delegatorsNumber; i++) {
+            const fStake = await StakingAuRa.instance.methods.stakeAmount(candidate, delegators[i]).call();
+            const fStakeBN = new BN(fStake.toString());
+            expect(fStakeBN, `Stake on candidate ${candidate} didn't increase`).to.be.bignumber.equal(minStakeBN);
         }
     });
 
