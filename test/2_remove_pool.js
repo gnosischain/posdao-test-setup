30031745a722262b07596363420860923e745209
phahulin <p.hahulin@hintsolutions.ru>
2019-10-11 11:05:54 +0300

    Add a test for unremovable validator. (#61)
    * Add a test for unremovable validator.
    
    * Fix tests for the unremovable validator

--- test/2_remove_pool.js
+++ test/2_remove_pool.js
@@ -13,31 +13,27 @@ const expect = require('chai')
     .expect;
 const ValidatorSetAuRa = require('../utils/getContract')('ValidatorSetAuRa', web3);
 const StakingAuRa = require('../utils/getContract')('StakingAuRa', web3);
-const StakingTokenContract = require('../utils/getContract')('StakingToken', web3);
-const sendInStakingWindow = require('../utils/sendInStakingWindow');
 const waitForValidatorSetChange = require('../utils/waitForValidatorSetChange');
 const pp = require('../utils/prettyPrint');
 
 describe('Pool removal and validator set change', () => {
     let tiredValidator = {};
 
-    it('Validator removes his pool', async () => {
+    it('The last validator removes his pool', async () => {
         let validators = await ValidatorSetAuRa.instance.methods.getValidators().call();
         console.log('***** Initial validator set = ' + JSON.stringify(validators));
         if (!validators.length == 1) {
             throw new Error('This test cannot be performed because it requires at least 2 validators in the validatorSet');
         }
 
-        tiredValidator.mining = validators[validators.length-1];
+        tiredValidator.mining = validators[validators.length - 1];
         tiredValidator.staking = await ValidatorSetAuRa.instance.methods.stakingByMiningAddress(tiredValidator.mining).call();
         console.log('***** Validator to be removed: ' + JSON.stringify(tiredValidator));
-        let tx = await sendInStakingWindow(web3, async () => {
-            return SnS(web3, {
-                from: tiredValidator.staking,
-                to: StakingAuRa.address,
-                method: StakingAuRa.instance.methods.removeMyPool(),
-                gasPrice: '1000000000',
-            });
+        const tx = await SnS(web3, {
+            from: tiredValidator.staking,
+            to: StakingAuRa.address,
+            method: StakingAuRa.instance.methods.removeMyPool(),
+            gasPrice: '1000000000'
         });
         pp.tx(tx);
         expect(tx.status, `Failed tx: ${tx.transactionHash}`).to.equal(true);
@@ -50,5 +46,65 @@ describe('Pool removal and validator set change', () => {
         expect(validatorIndex, `Validator ${JSON.stringify(tiredValidator)}
             removed his pool but still is in validator set`)
             .to.equal(-1);
+    });
+});
+
+describe('Unremovable validator removes his pool', () => {
+    let unremovableValidator = {};
+
+    it('Owner calls ValidatorSetAuRa.clearUnremovableValidator', async () => {
+        unremovableValidator.staking = (await ValidatorSetAuRa.instance.methods.unremovableValidator().call()).toLowerCase();
+        unremovableValidator.mining = (await ValidatorSetAuRa.instance.methods.miningByStakingAddress(unremovableValidator.staking).call()).toLowerCase();
+
+        if (unremovableValidator.staking == '0x0000000000000000000000000000000000000000') {
+            console.log('***** Unremovable validator doesn\'t exist. Skip this test');
+            return;
+        }
+
+        const validatorsList = await ValidatorSetAuRa.instance.methods.getValidators().call();
+        expect(validatorsList[0].toLowerCase() == unremovableValidator.mining, `Unexpected unremovable validator mining: ${validatorsList[0]}, actual mining: ${unremovableValidator.mining}`)
+            .to.equal(true);
+
+        console.log('***** Unremovable validator: ' + JSON.stringify(unremovableValidator));
+        console.log('***** OWNER calls clearUnremovableValidator');
+        const tx = await SnS(web3, {
+            from: OWNER,
+            to: ValidatorSetAuRa.address,
+            method: ValidatorSetAuRa.instance.methods.clearUnremovableValidator(),
+            gasPrice: '0',
+        });
+        pp.tx(tx);
+        expect(tx.status, `Failed tx: ${tx.transactionHash}`).to.equal(true);
+
+        const check_unremovableValidator = await ValidatorSetAuRa.instance.methods.unremovableValidator().call();
+        console.log('***** ValidatorSetAuRa.unremovableValidator after the call: ' + check_unremovableValidator);
+        expect(check_unremovableValidator == '0x0000000000000000000000000000000000000000', 'Unremovable validator is not cleared')
+            .to.equal(true);
+    });
+
+    it('Ex unremovable validator removes his pool', async() => {
+        if (unremovableValidator.staking == '0x0000000000000000000000000000000000000000') {
+            console.log('***** Unremovable validator doesn\'t exist. Skip this test');
+            return;
+        }
+
+        console.log('***** Ex unremovable validator calls StakingAuRa.removeMyPool');
+        const tx = await SnS(web3, {
+            from: unremovableValidator.staking,
+            to: StakingAuRa.address,
+            method: StakingAuRa.instance.methods.removeMyPool(),
+            gasPrice: '1000000000'
+        });
+        pp.tx(tx);
+        expect(tx.status, `Failed tx: ${tx.transactionHash}`).to.equal(true);
+
+        const pools = await StakingAuRa.instance.methods.getPools().call();
+        const poolFound = pools.filter(pool => pool.toLowerCase() == unremovableValidator.staking).length > 0;
+        expect(poolFound, 'Unremovable validator\'s pool still exists').to.equal(false);
+
+        const validatorsList = await waitForValidatorSetChange(web3);
+        console.log('***** Validators list after removal = ' + JSON.stringify(validatorsList));
+        expect(validatorsList[0].toLowerCase() != unremovableValidator.mining, `Unremovable validator still present in the validators list`)
+            .to.equal(true);
     });
 });
