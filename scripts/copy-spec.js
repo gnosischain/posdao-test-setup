b947be4779cba941191ea7664020f4fcf5eafce0
varasev <33550681+varasev@users.noreply.github.com>
2019-12-05 16:09:37 +0300

    Remove temporary patch

--- scripts/copy-spec.js
+++ scripts/copy-spec.js
@@ -10,12 +10,6 @@ async function main() {
   specFile = JSON.parse(specFile);
   assert(specFile.engine.authorityRound.params.stepDuration != null);
   specFile.engine.authorityRound.params.stepDuration = 4;
-  
-  // This is a temporary patch until `randomness_contract_address` option
-  // in https://github.com/poanetwork/parity-ethereum/tree/aura-pos
-  // is changed from `Option<Address>` to `Option<BTreeMap<Uint, Address>>`
-  specFile.engine.authorityRound.params.randomnessContractAddress = specFile.engine.authorityRound.params.randomnessContractAddress[0];
-  
   await promisify(fs.writeFile)(__dirname + '/../parity-data/spec.json', JSON.stringify(specFile, null, '  '), 'UTF-8');
 }
 
